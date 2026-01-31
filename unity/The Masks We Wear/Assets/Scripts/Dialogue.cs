33ea99a10c244bacf2cb645becf6cf1ff3e96637
Kaiden <134072375+Ufighter34@users.noreply.github.com>
2026-01-31 15:22:11 -0500

    Added dialogue
    Added dialogue and made it so that the player cannot click it until the person shows on screen

---
+++ unity/The Masks We Wear/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+
+    public TextMeshProUGUI text;
+    public string[] sentences;
+    public float typingSpeed;
+    private int index;
+    public GameObject dialogueBox;
+    public MovePerson person;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        text.text = string.Empty;
+
+
+        //StartDialogue();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartDialogue()
+    {
+        if (person.reachedTarget)
+        {
+            index = 0;
+            dialogueBox.SetActive(true);
+            //doesn't run a method all at one. Does one step, takes a break, then continues to the next step
+            StartCoroutine(TypeLine());
+        }
+    }
+
+    void NextLine()
+    {
+
+    }
+
+    //IEnumerator is used to iterate through data one thing at a time
+    public IEnumerator TypeLine()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            text.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        Debug.Log("Finished Typing");
+    }
+}
