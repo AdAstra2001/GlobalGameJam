fd11b1d0942a0d0317a2f18539af880fa5fc3c9f
SavageSwopes <jacksonswopes@gmail.com>
2026-02-01 14:36:07 -0500

    Moved scenes around

--- unity/The Masks We Wear/Assets/Scripts/MovePerson.cs
+++ unity/The Masks We Wear/Assets/Scripts/MovePerson.cs
@@ -4,15 +4,14 @@ public class MovePerson : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float moveSpeed = 6f;
-    private Vector3 targetPosition;
+    public Vector3 targetPosition;
     private static Vector3 basePosition;
     public bool reachedTarget = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         basePosition = transform.position;
-        targetPosition = transform.position;
-        targetPosition = new Vector3((float)-3.08, (float)0.37, 0);//Edit this to the position of the player head
+        //Edit this to the position of the player head
     }
 
     // Update is called once per frame
