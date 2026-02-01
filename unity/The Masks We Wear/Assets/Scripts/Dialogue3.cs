9d08a6a3825975a4fbf960d07769d5eea5d7824c
SavageSwopes <jacksonswopes@gmail.com>
2026-02-01 10:38:37 -0500

    Messed with scene 1

---
+++ unity/The Masks We Wear/Assets/Scripts/Dialogue3.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialog3 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void OnClick()
+    {
+        Debug.Log("Clicked");
+    }
+}
